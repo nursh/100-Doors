f5a10b1e33e036dc0416e77484b369be5ae132a1
nur <nuradeen@nur-sh.me>
2015-11-08 20:42:25 -0500

    added  door status

--- src/com/nursh/DoorStatus.java
+++ src/com/nursh/DoorStatus.java
@@ -1,3 +1,19 @@
 package com.nursh;
 public enum DoorStatus {
+    Open("Open"),
+    Close("Close");
+
+
+    private String status;
+
+
+    DoorStatus(String status){
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+
 }
