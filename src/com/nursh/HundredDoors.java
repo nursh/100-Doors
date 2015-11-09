0c689c4075f3d0540d19559cb5a64ba07b1474d6
nur <nuradeen@nur-sh.me>
2015-11-08 21:58:53 -0500

    logic of going through the doors

--- src/com/nursh/HundredDoors.java
+++ src/com/nursh/HundredDoors.java
@@ -7,9 +7,12 @@ public class HundredDoors {
 
     private static final int MAX_DOORS = 100;
     private Map<Integer, DoorStatus> Doors;
+    private int doorLevel;
 
     public HundredDoors(){
         Doors = new HashMap<>();
+        doorLevel = 1;
+        this.insertDoors();
     }
 
     private void insertDoors() {
@@ -18,4 +21,35 @@ public class HundredDoors {
             Doors.put(door, DoorStatus.Close);
         }
     }
+
+    public void play() {
+        while (doorLevel <= MAX_DOORS) {
+            for (int door = doorLevel; door <= MAX_DOORS; ) {
+                this.changeDoorStatus(door);
+                door = getNextDoor(door);
+            }
+            doorLevel++;
+        }
+    }
+
+    private int getNextDoor(int presentDoor) {
+        return presentDoor + doorLevel;
+    }
+
+    private void changeDoorStatus(int door){
+        if(Doors.get(door) == DoorStatus.Close ){
+            Doors.put(door, DoorStatus.Open);
+        } else {
+            Doors.put(door, DoorStatus.Close);
+        }
+    }
+
+    public void print() {
+        System.out.println("After 100 runs:  ");
+        for(int door = 1; door <= Doors.size(); door++){
+            if(Doors.get(door).getStatus().equals("Opened"))
+                System.out.println("Door " + door + " is " + Doors.get(door).getStatus());
+        }
+    }
+
 }
