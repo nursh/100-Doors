39e4c6178147211d73f81afc02e2db0e3ac6920c
nur <nuradeen@nur-sh.me>
2015-11-08 21:59:24 -0500

    main method finished

--- src/com/nursh/Main.java
+++ src/com/nursh/Main.java
@@ -3,6 +3,8 @@ package com.nursh;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        HundredDoors hd = new HundredDoors();
+        hd.play();
+        hd.print();
     }
 }
